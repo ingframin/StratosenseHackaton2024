5765de462dcf645365d99851e327e7ff9eb58e04
framin2@gmail.com <framin2@gmail.com>
2024-11-11 16:08:39 +0100

    data with repetitions

--- generate_samples/gen_fsk_signal.py
+++ generate_samples/gen_fsk_signal.py
@@ -1,5 +1,5 @@
 from modulations import *
-from random import randint
+from random import randint, choice
 import numpy as np
 
 messages = [np.array([randint(0,255) for _ in range(32)]) for _ in range(8)]
@@ -7,8 +7,10 @@ result = [fsk_modulate(m) for m in messages]
 
 
 result = []
-for m in messages:
-    result.append(m)
+
+for _ in range(256):
+
+    result.append(choice(messages))
     result.append(np.zeros(randint(16,128)))
 
 final = np.concatenate(result)
