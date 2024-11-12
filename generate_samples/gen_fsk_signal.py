487543ec05fba5ed17905591e61f5a9ccd76f53a
ingframin <framin2@gmail.com>
2024-11-12 12:57:22 +0100

    final version of gen fsk data

--- generate_samples/gen_fsk_signal.py
+++ generate_samples/gen_fsk_signal.py
@@ -2,36 +2,22 @@ from modulations import *
 from random import randint, choice, random
 import numpy as np
 from channel import *
-import shelve
-import gc
+import h5py
 
-class Message:
-    id = 0
-    def __init__(self, mod:callable) -> None:
-        self.id = Message.id
-        Message.id += 1
-        self.content = np.array([randint(0,255) for _ in range(512)])
-        self.modulated_content = mod(self.content)
-        self.variations = []
-    
-    def compute_variations(self,delays,attenuations,f=433.9e6):
-        for d,a in zip(delays,attenuations):
-            self.variations.append(add_delay(add_attenuation(self.modulated_content,a),d,f))
-    
 
-             
+messages = (np.array([randint(0,255) for _ in range(16)]) for _ in range(32))             
+modulated_messages = (fsk_modulate(m,f1=5,f2=10) for id,m in enumerate(messages))
 delays = [0.1*random() for _ in range(16)]
 attenuations = [random() for _ in range(16)]
 
-with shelve.open("fsk_dataset") as dataset:
-    for i in range(1000):
-        m = Message(fsk_modulate)
-        print("Message: ",m.id)
-        m.compute_variations(delays,attenuations)
-        print("variations: ",len(m.variations))
-        dataset[str(m.id)] = m
-        gc.collect()
+with h5py.File('fsk_data.h5', 'w') as f:
+    for i,m in enumerate(modulated_messages):
+        print(f'Message: {i}')
+        data = np.zeros((16, len(m)),dtype=np.complex64)
+        for a,d,n in zip(attenuations,delays,range(16)):
+            data[n] = add_attenuation(add_delay(m,d,433e6),a)
+        f.create_dataset(f'{i}',data=data)
 
 
-
-
+    
+    
